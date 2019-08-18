aeda94fec7aacacb442d24f424b47a3acd3fd314
Osse Laborde <oml007@live.com>
2019-08-17 23:18:24 -0400

    added exception

---
+++ spring-boot-exception/spring-boot-exception/src/main/java/com/laborde/springbootexception/controller/RoleController.java
@@ -0,0 +1,15 @@
+package com.laborde.springbootexception.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RoleController {
+
+
+    @RequestMapping("/roleadd")
+    public String add(){
+        int num = 10 / 0;
+        return "add";
+    }
+}
